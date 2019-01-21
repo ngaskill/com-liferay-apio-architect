f9fe7505ae84b829dadef45f244c36aba80e36a3
Nick Gaskill <nicholas.gaskill@liferay.com>
2019-01-21 15:36:45 -0600

    LRDOCS-6141 Javadoc review

--- apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/action/ActionSemantics.java
+++ apio-architect-impl/src/main/java/com/liferay/apio/architect/internal/action/ActionSemantics.java
@@ -44,17 +44,18 @@ import java.util.stream.Stream;
 import javax.ws.rs.ForbiddenException;
 
 /**
- * Instances of this class contains semantic information about an action like
+ * Contains semantic information about an action.
  *
  * @author Alejandro Hernández
- * @review
  */
 public final class ActionSemantics {
 
 	/**
-	 * Starts creating a new {@link ActionSemantics} by providing information
-	 * about the action's resource.
+	 * Starts creating a new {@code ActionSemantics} instance by providing
+	 * information about the action's resource.
 	 *
+	 * @param  resource the action's resource
+	 * @return the {@code NameStep}
 	 * @review
 	 */
 	public static NameStep ofResource(Resource resource) {
@@ -66,9 +67,12 @@ public final class ActionSemantics {
 	}
 
 	/**
-	 * Executes the permission function with the provided params to check if we
-	 * have permissions to execute an action
+	 * Executes the permission function with the provided parameters to check if
+	 * the user has the permissions to execute an action.
 	 *
+	 * @param  params the parameters
+	 * @return {@code true} if the user has the necessary permissions; {@code
+	 *         false} otherwise
 	 * @review
 	 */
 	public Boolean checkPermissions(List<?> params) throws Throwable {
@@ -76,8 +80,10 @@ public final class ActionSemantics {
 	}
 
 	/**
-	 * Executes the action with the provided params.
+	 * Executes the action with the provided parameters.
 	 *
+	 * @param  params the parameters
+	 * @return the object
 	 * @review
 	 */
 	public Object execute(List<?> params) throws Throwable {
@@ -85,28 +91,29 @@ public final class ActionSemantics {
 	}
 
 	/**
-	 * The action's name.
+	 * Returns the action's name.
 	 *
-	 * @review
+	 * @return the action's name
 	 */
 	public String getActionName() {
 		return _name;
 	}
 
 	/**
-	 * The list of annotations.
+	 * Returns the list of annotations.
 	 *
-	 * @review
+	 * @return the list of annotations
 	 */
 	public List<Annotation> getAnnotations() {
 		return unmodifiableList(_annotations);
 	}
 
 	/**
-	 * Returns the transformed body value needed for the action, if needed.
-	 * Returns {@code null} otherwise.
+	 * Returns the transformed body value for the action, if needed. Returns
+	 * {@code null} otherwise.
 	 *
-	 * @review
+	 * @param  body the body
+	 * @return the transformed body value, if needed; {@code null} otherwise
 	 */
 	public Object getBodyValue(Body body) {
 		if (_bodyFunction == null) {
@@ -120,25 +127,25 @@ public final class ActionSemantics {
 	 * Returns the form for the action, if present. Returns {@code
 	 * Optional#empty()} otherwise.
 	 *
-	 * @review
+	 * @return the form, if present; {@code Optional#empty()} otherwise
 	 */
 	public Optional<Form> getFormOptional() {
 		return Optional.ofNullable(_form);
 	}
 
 	/**
-	 * The method in which the action is executed.
+	 * Returns the method in which the action is executed.
 	 *
-	 * @review
+	 * @return the method
 	 */
 	public String getHTTPMethod() {
 		return _method;
 	}
 
 	/**
-	 * The list of param classes.
+	 * Returns the list of parameter classes.
 	 *
-	 * @review
+	 * @return the list of parameter classes
 	 */
 	public List<Class<?>> getParamClasses() {
 		return unmodifiableList(_paramClasses);
@@ -175,38 +182,38 @@ public final class ActionSemantics {
 	}
 
 	/**
-	 * The list of permission classes.
+	 * Returns the list of permission classes.
 	 *
-	 * @review
+	 * @return the list of permission classes
 	 */
 	public List<Class<?>> getPermissionProvidedClasses() {
 		return unmodifiableList(_permissionProvidedClasses);
 	}
 
 	/**
-	 * The action's resource.
+	 * Returns the action's resource.
 	 *
-	 * @review
+	 * @return the action's resource
 	 */
 	public Resource getResource() {
 		return _resource;
 	}
 
 	/**
-	 * The class returned by the action.
+	 * Returns the class returned by the action.
 	 *
-	 * @review
+	 * @return the class returned by the action
 	 */
 	public Class<?> getReturnClass() {
 		return _returnClass;
 	}
 
 	/**
-	 * Transforms this {@link ActionSemantics} instance into its {@link Action}.
+	 * Transforms this {@code ActionSemantics} instance into its {@code Action}.
 	 *
 	 * @param  provideFunction the function used to provide instances of action
-	 *         params
-	 * @review
+	 *         parameters
+	 * @return the action
 	 */
 	@SuppressWarnings({"Convert2MethodRef", "unchecked"})
 	public Action toAction(ProvideFunction provideFunction) {
@@ -234,14 +241,14 @@ public final class ActionSemantics {
 	}
 
 	/**
-	 * Copies the current {@link ActionSemantics} by setting a value for the
-	 * {@link ActionSemantics#getAnnotations()} annotations} attribute. A
-	 * shallow reference equality check is used to prevent copying of the same
-	 * value by returning {@code this}.
+	 * Copies the current {@code ActionSemantics} by setting a new value for the
+	 * annotations attribute. If the same annotations value is provided, a
+	 * shallow reference equality check prevents the copy and returns {@code
+	 * this}.
 	 *
 	 * @param  annotations the new annotations list
-	 * @return A modified copy of {@code this} object
-	 * @review
+	 * @return the copy of the current {@code ActionSemantics} with new
+	 *         annotations; {@code this} if the annotations are the same
 	 */
 	public ActionSemantics withAnnotations(List<Annotation> annotations) {
 		if (_annotations.equals(annotations)) {
@@ -267,14 +274,13 @@ public final class ActionSemantics {
 	}
 
 	/**
-	 * Copies the current {@link ActionSemantics} by setting a value for the
-	 * {@link ActionSemantics#getHTTPMethod()} method} attribute. A shallow
-	 * reference equality check is used to prevent copying of the same value by
-	 * returning {@code this}.
+	 * Copies the current {@code ActionSemantics} by setting a new value for the
+	 * HTTP method attribute. If the same HTTP method is provided, a shallow
+	 * reference equality check prevents the copy and returns {@code this}.
 	 *
-	 * @param  method the new method
-	 * @return A modified copy of {@code this} object
-	 * @review
+	 * @param  method the new HTTP method
+	 * @return the copy of the current {@code ActionSemantics} with the new HTTP
+	 *         method; {@code this} if the HTTP method is the same
 	 */
 	public ActionSemantics withMethod(String method) {
 		if (_method.equals(method)) {
@@ -300,14 +306,13 @@ public final class ActionSemantics {
 	}
 
 	/**
-	 * Copies the current {@link ActionSemantics} by setting a value for the
-	 * {@link ActionSemantics#getActionName()} name} attribute. A shallow
-	 * reference equality check is used to prevent copying of the same value by
-	 * returning {@code this}.
+	 * Copies the current {@code ActionSemantics} by setting a new value for the
+	 * name attribute. If the same name is provided, a shallow reference
+	 * equality check prevents the copy and returns {@code this}.
 	 *
 	 * @param  name the new name
-	 * @return A modified copy of {@code this} object
-	 * @review
+	 * @return the copy of the current {@code ActionSemantics} with the new
+	 *         name; {@code this} if the name is the same
 	 */
 	public ActionSemantics withName(String name) {
 		if (_name.equals(name)) {
@@ -333,14 +338,13 @@ public final class ActionSemantics {
 	}
 
 	/**
-	 * Copies the current {@link ActionSemantics} by setting a value for the
-	 * {@link ActionSemantics#getResource() resource} attribute. A shallow
-	 * reference equality check is used to prevent copying of the same value by
-	 * returning {@code this}.
+	 * Copies the current {@link ActionSemantics} by setting a new value for the
+	 * resource attribute. If the same resource is provided, a shallow reference
+	 * equality check prevents the copy and returns {@code this}.
 	 *
 	 * @param  resource the new resource
-	 * @return A modified copy of {@code this} object
-	 * @review
+	 * @return the copy of the current {@code ActionSemantics} with the new
+	 *         resource; {@code this} if the resource is the same
 	 */
 	public ActionSemantics withResource(Resource resource) {
 		ActionSemantics actionSemantics = new ActionSemantics();
@@ -362,14 +366,13 @@ public final class ActionSemantics {
 	}
 
 	/**
-	 * Copies the current {@link ActionSemantics} by setting a value for the
-	 * {@link ActionSemantics#getReturnClass()} return class} attribute. A
-	 * shallow reference equality check is used to prevent copying of the same
-	 * value by returning {@code this}.
+	 * Copies the current {@link ActionSemantics} by setting a new value for the
+	 * return class attribute. If the same return class is provided, a shallow
+	 * reference equality check prevents the copy and returns {@code this}.
 	 *
 	 * @param  returnClass the new return class
-	 * @return A modified copy of {@code this} object
-	 * @review
+	 * @return the copy of the current {@code ActionSemantics} with the new
+	 *         return class; {@code this} if the return class is the same
 	 */
 	public ActionSemantics withReturnClass(Class<?> returnClass) {
 		if (_returnClass.equals(returnClass)) {
@@ -504,9 +507,11 @@ public final class ActionSemantics {
 
 		/**
 		 * Provides information about the function action's execute function.
-		 * This function receives the list of params in the order provided in
-		 * the {@link FinalStep#receivesParams(Class[])} method.
+		 * This function receives the list of parameters in the order provided
+		 * in the {@link FinalStep#receivesParams(Class[])} method.
 		 *
+		 * @param  executeCheckedFunction1 the execute function
+		 * @return the {@code FinalStep}
 		 * @review
 		 */
 		public FinalStep executeFunction(
@@ -514,9 +519,11 @@ public final class ActionSemantics {
 
 		/**
 		 * Provides information about the permission method arguments. This
-		 * function receives the list of param classes to provide to the
-		 * permissionCheckedFunction1
+		 * function receives the list of parameter classes to provide to the
+		 * {@code permissionCheckedFunction1}.
 		 *
+		 * @param  classes the list of parameter classes
+		 * @return the {@code ExecuteStep}
 		 * @review
 		 */
 		public ExecuteStep permissionProvidedClasses(Class<?>... classes);
@@ -526,68 +533,76 @@ public final class ActionSemantics {
 	public interface FinalStep {
 
 		/**
-		 * Provides information about the params needed by the action.
+		 * Provides information about the parameters the action needs.
 		 *
 		 * <p>
-		 * The param instances will be provided in the {@link #execute(List)} in
-		 * the same order as their classes in this method. {@link Void} classes
-		 * will be ignored (will be provided as {@code null}. For the {@link Id}
-		 * or {@link com.liferay.apio.architect.annotation.ParentId} params, the
+		 * The parameter instances must be provided to the method {@link
+		 * ActionSemantics#execute(List)} in the same order as their classes in
+		 * this method. {@code Void} classes will be provided as {@code null}
+		 * and ignored. For the {@code Id} or {@link
+		 * com.liferay.apio.architect.annotation.ParentId} parameters, the
 		 * annotation class should be provided to the list.
 		 * </p>
 		 *
+		 * @param  annotations the annotations
+		 * @return the {@code FinalStep}
 		 * @review
 		 */
 		public FinalStep annotatedWith(Annotation annotations);
 
 		/**
-		 * Provides information about the params needed by the action.
+		 * Provides information about the parameters the action needs.
 		 *
 		 * <p>
-		 * The param instances will be provided in the {@link #execute(List)} in
-		 * the same order as their classes in this method. {@link Void} classes
-		 * will be ignored (will be provided as {@code null}. For the {@link Id}
-		 * or {@link com.liferay.apio.architect.annotation.ParentId} params, the
+		 * The parameter instances must be provided to the method {@link
+		 * ActionSemantics#execute(List)} in the same order as their classes in
+		 * this method. {@code Void} classes will be provided as {@code null}
+		 * and ignored. For the {@code Id} or {@link
+		 * com.liferay.apio.architect.annotation.ParentId} parameters, the
 		 * annotation class should be provided to the list.
 		 * </p>
 		 *
+		 * @param  annotations the annotations
+		 * @return the {@code FinalStep}
 		 * @review
 		 */
 		public FinalStep annotatedWith(Annotation... annotations);
 
 		/**
-		 * Creates the {@link ActionSemantics} object with the information
+		 * Creates the {@code ActionSemantics} object with the information
 		 * provided to the builder.
 		 *
-		 * @review
+		 * @return the {@code ActionSemantics} object
 		 */
 		public ActionSemantics build();
 
 		/**
 		 * Provides information about the form and function used to transform
-		 * the body value into the object needed by the action.
+		 * the body value into the object the action needs. Don't call this
+		 * method if the action doesn't need information from the body.
 		 *
-		 * <p>
-		 * If the action does not need information from the body, this method
-		 * shouldn't be called.
-		 * </p>
-		 *
+		 * @param  form the form
+		 * @param  function2 the function
+		 * @return the {@code FinalStep}
 		 * @review
 		 */
 		public FinalStep form(
 			Form form, Function2<Form, Body, Object> function2);
 
 		/**
-		 * Provides information about the params needed by the action.
+		 * Provides information about the parameters the action needs.
 		 *
 		 * <p>
-		 * The param instances will be provided in the {@link #execute(List)} in
-		 * the same order as their classes in this method. {@link Void} classes
-		 * will be ignored (will be provided as {@code null}. For the {@link Id}
-		 * or {@link com.liferay.apio.architect.annotation.ParentId} params, the
+		 * The parameter instances must be provided to the method {@link
+		 * ActionSemantics#execute(List)} in the same order as their classes in
+		 * this method. {@code Void} classes will be provided as {@code null}
+		 * and ignored. For the {@code Id} or {@link
+		 * com.liferay.apio.architect.annotation.ParentId} parameters, the
 		 * annotation class should be provided to the list.
 		 * </p>
 		 *
+		 * @param  classes the classes
+		 * @return the {@code FinalStep}
 		 * @review
 		 */
 		public FinalStep receivesParams(Class<?>... classes);
@@ -597,9 +612,11 @@ public final class ActionSemantics {
 	public interface MethodStep {
 
 		/**
-		 * Provides information about the method in which the action is
-		 * executed.
+		 * Provides information about the method (supplied to this method as an
+		 * {@code HTTPMethod}) in which the action is executed.
 		 *
+		 * @param  httpMethod the method
+		 * @return the {@code ReturnStep}
 		 * @review
 		 */
 		public default ReturnStep method(HTTPMethod httpMethod) {
@@ -607,9 +624,11 @@ public final class ActionSemantics {
 		}
 
 		/**
-		 * Provides information about the method in which the action is
-		 * executed.
+		 * Provides information about the method (supplied to this method as a
+		 * {@code String}) in which the action is executed.
 		 *
+		 * @param  method the method
+		 * @return the {@code ReturnStep}
 		 * @review
 		 */
 		public ReturnStep method(String method);
@@ -619,8 +638,10 @@ public final class ActionSemantics {
 	public interface NameStep {
 
 		/**
-		 * Provides information about the the action's name.
+		 * Provides information about the action's name.
 		 *
+		 * @param  name the action's name
+		 * @return the {@code MethodStep}
 		 * @review
 		 */
 		public MethodStep name(String name);
@@ -630,8 +651,9 @@ public final class ActionSemantics {
 	public interface PermissionStep {
 
 		/**
-		 * Default empty implementation of the permission function.
+		 * Returns the default empty implementation of the permission function.
 		 *
+		 * @return the {@code ExecuteStep}
 		 * @review
 		 */
 		public ExecuteStep permissionFunction();
@@ -640,6 +662,8 @@ public final class ActionSemantics {
 		 * Provides information about the permission function to check if the
 		 * execute function has permissions to be executed.
 		 *
+		 * @param  permissionCheckedFunction1 the permission function
+		 * @return the {@code ExecuteStep}
 		 * @review
 		 */
 		public ExecuteStep permissionFunction(
@@ -652,6 +676,8 @@ public final class ActionSemantics {
 		/**
 		 * Provides information about the class returned by the action.
 		 *
+		 * @param  returnClass the class returned by the action
+		 * @return the {@code PermissionStep}
 		 * @review
 		 */
 		public PermissionStep returns(Class<?> returnClass);
